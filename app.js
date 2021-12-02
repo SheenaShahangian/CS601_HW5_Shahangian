cdfbbf489e29938ac06dd32c0193b49b3730c194
sheenashahangian <sheenas@bu.edu>
2021-12-01 23:39:37 -0700

    testing

--- app.js
+++ app.js
@@ -2,33 +2,35 @@
 // url for the JSON file
 let degreesUrl = "https://serene-wing-4e1877.netlify.app/degrees.json";
 
-// fetch the data in the JSON file
-fetch(degreesUrl).then(
-    (response) => {
-        // log the status
-        console.log(`Status ${response.status}`);
+async function fetchDegreeData() {
+    // fetch the data in the JSON file
+    await fetch(degreesUrl).then(
+        (response) => {
+            // log the status
+            console.log(`Status ${response.status}`);
 
-        // return the JSON object
-        return response.json();
-    }
-).then(
-    (data) => {
-        console.log("DEGREE ONE");
-        console.log(data.my_degrees[0].degree.school);
-        console.log(data.my_degrees[0].degree.major);
-        console.log(data.my_degrees[0].degree.type);
-        console.log(data.my_degrees[0].degree.year);
+            // return the JSON object
+            return response.json();
+        }
+    ).then(
+        (data) => {
+            console.log("DEGREE ONE");
+            console.log(data.my_degrees[0].degree.school);
+            console.log(data.my_degrees[0].degree.major);
+            console.log(data.my_degrees[0].degree.type);
+            console.log(data.my_degrees[0].degree.year);
 
-        console.log("DEGREE TWO");
-        console.log(data.my_degrees[1].degree.school);
-        console.log(data.my_degrees[1].degree.major);
-        console.log(data.my_degrees[1].degree.type);
-        console.log(data.my_degrees[1].degree.year);
+            console.log("DEGREE TWO");
+            console.log(data.my_degrees[1].degree.school);
+            console.log(data.my_degrees[1].degree.major);
+            console.log(data.my_degrees[1].degree.type);
+            console.log(data.my_degrees[1].degree.year);
 
-    }
-)
-// error message if there's an issue fetching data
-.catch(error => {
-    console.log("Bummer! There was an issue fetching your data!");
-})
+        }
+    )
+    // error message if there's an issue fetching data
+    .catch(error => {
+        console.log("Bummer! There was an issue fetching your data!");
+    })
+}
 
