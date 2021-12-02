f6530bd7a47df1c0d4d6639bc707cb0827d7f4ca
sheenashahangian <sheenas@bu.edu>
2021-12-01 22:08:50 -0700

    start from scratch

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
+
+// url for the JSON file
+let degreesUrl = "https://serene-wing-4e1877.netlify.app/degrees.json";
 // async function makeRequest(url) {
 //     // fetch the url
 //     await fetch(url)
@@ -26,34 +29,18 @@
 
 // let degreeData = makeRequest("https://serene-wing-4e1877.netlify.app/degrees.json");
 
-function tester() {
-    console.log("hey, we're here");
-    console.log(makeRequest());
-    console.log("hey, we're done");
-}
+// function tester() {
+//     console.log("hey, we're here");
+//     console.log(makeRequest());
+//     console.log("hey, we're done");
+// }
 
-async function makeRequest() {
-    await fetch('https://serene-wing-4e1877.netlify.app/degrees.json')
-        .then(response => 
-            // check the status, and if it's not okay, throw an error
-            //if (!response.ok) {
-                //throw new Error("Status Issue " + response.status);
-            //}
-            response.json()
-        .then(data => ({status: response.status, body: data})))
-        // .then(function(response) {
+// async function makeRequest() {
+//     await fetch(degreesUrl)
+//         .then(response => 
+//             response.json()
+//         .then(data => ({status: response.status, body: data})))
+//         //.then(data => console.log(data));
+// }
 
-        //     // print status to the console
-        //     console.log(`Status: ${response.status}`);
-            
-        //     // check the status, and proceed if status is ok
-        //     if (response.status === 200) {
-        //     response.json();
-        //     }
-        //     // if status is not ok, print error message to the console
-        //     else {
-        //         console.log("Sorry, there was an error when fetching your data!")
-        //     }
-        // })
-        //.then(data => console.log(data));
-}
+console.log(fetch(degreesUrl));
