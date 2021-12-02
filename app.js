405e855645521a0e9fd140d93088e09337269eee
sheenashahangian <sheenas@bu.edu>
2021-12-01 20:39:38 -0700

    WIP

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ async function makeRequest(url) {
     await fetch(url)
         // the ".then" listed below will run when we get a response from the server
         .then(function(response) {
+
             // print status to the console
             console.log(`Status: ${response.status}`);
+
             // check the status, and proceed if status is ok
             if (response.status === 200) {
                 // return JSON object
+                console.log("this is response");
+                console.log(response.json());
                 return response.json();
             }
             // if status is not ok, print error message to the console
